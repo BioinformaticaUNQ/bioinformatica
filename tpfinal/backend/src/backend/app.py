18c7fa7e9d0c368aa0fa301f25df42670d54b721
Fabrizio Britez <fabri1108@gmail.com>
2020-11-14 23:23:43 -0300

    add homologous sequences endpoint

--- tpfinal/backend/src/backend/app.py
+++ tpfinal/backend/src/backend/app.py
@@ -3,9 +3,8 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from src.backend.validators.pdbValidators import PdbValidators
 from src.backend.sequence.utils import get_sequence_from
+from src.backend.sequence.blastUtils import blast_records
 import json
-
-from Bio.Blast import NCBIWWW, NCBIXML
 
 app = Flask(__name__)
 CORS(app, suppport_credentials=True)
@@ -19,42 +18,44 @@ def pdb_code():
 
 
 @app.route('/sequence', methods=['POST'])
+@cross_origin(support_credentials=True)
 def sequence():
     pdb_code = request.form['pdbcode']
     PdbValidators.validate_pdb_code(pdb_code)
-    sequence = ''
 
-    if PdbValidators.validate_pdb_code(pdb_code):
-        sequence = get_sequence_from(pdb_code)
-
-    print(sequence)
-    return sequence
+    return get_sequence_from(pdb_code) if PdbValidators.validate_pdb_code(pdb_code) else ''
 
 @app.route('/homologousSequence', methods=['POST'])
+@cross_origin(support_credentials=True)
 def homologous_sequences():
+    sequences = homologous_sequences_details()
+    result = [{'title': seq.get('title'), 'sequence': seq.get('sequence')} for seq in sequences.get('d')]
+    return {'d': result}
+
+@app.route('/homologousSequenceDetails', methods=['POST'])
+@cross_origin(support_credentials=True)
+def homologous_sequences_details():
     main_sequence = sequence();
+    print(main_sequence)
 
-    blast_result_1 = NCBIWWW.qblast(
-        "blastp",
-        "pdb",
-        main_sequence,
-        short_query=True,
-        hitlist_size=50,
-        expect=1000,
-        word_size=7,
-        nucl_reward=1,
-        matrix_name='BLOSUM62',
-        nucl_penalty=-3,
-        threshold=0.05,
-        gapcosts="11 1",
-    )
+    records = blast_records(main_sequence)
 
-    blast_records = NCBIXML.read(blast_result_1)
-    for alignment in blast_records.alignments:
+    results = []
+    for alignment in records.alignments:
+        aligmened_sequence = ''
+        aligmened_matches = ''
         for hsp in alignment.hsps:
-            print(hsp)
+            aligmened_sequence = aligmened_sequence + hsp.sbjct
+            aligmened_matches = aligmened_matches + hsp.match
 
-    return 'Yeap'
+        results.append({
+            'title': alignment.title.split('>')[0],
+            'sequence' : aligmened_sequence.replace('-', ''),
+            'aligmened_matches': aligmened_matches,
+            'aligmened_sequence': aligmened_sequence
+        })
+
+    return {'d': results}
 
 
 if __name__ == '__main__':
---
+++ tpfinal/backend/src/backend/sequence/blastUtils.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+
+from Bio.Blast import NCBIWWW, NCBIXML
+
+
+@lru_cache
+def blast_records(sequence):
+    blast_result = NCBIWWW.qblast(
+        "blastp",
+        "pdb",
+        sequence,
+        short_query=True,
+        hitlist_size=50,
+        expect=1000,
+        word_size=7,
+        nucl_reward=1,
+        matrix_name='BLOSUM62',
+        nucl_penalty=-3,
+        threshold=0.05,
+        gapcosts="11 1",
+    )
+
+    return NCBIXML.read(blast_result)
