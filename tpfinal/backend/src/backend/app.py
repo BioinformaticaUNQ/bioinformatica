1e0ed65249df2c846505e0b79f9045bcd3b2aa4d
Lucas Emiliano Avalos <l.e.avalos1@gmail.com>
2020-11-27 18:17:02 -0300

    Cambio form por json para que funcione Flask

--- tpfinal/backend/src/backend/app.py
+++ tpfinal/backend/src/backend/app.py
@@ -4,26 +4,26 @@ from flask_cors import CORS, cross_origin
 from src.backend.validators.pdbValidators import PdbValidators
 from src.backend.sequence.utils import get_sequence_from
 from src.backend.sequence.blastUtils import blast_records
-import json
+
 
 app = Flask(__name__)
 CORS(app, suppport_credentials=True)
 
+
 @app.route('/pdbCode', methods=['POST'])
 @cross_origin(support_credentials=True)
 def pdb_code():
-    data = request.form
-    PdbValidators.validate_pdb_code(data['pdbcode'])
-    return PdbValidators.validate_pdb_code(data['pdbcode'])
+    pdb_code = request.json['pdbcode']
+    return PdbValidators.validate_pdb_code(pdb_code)
 
 
 @app.route('/sequence', methods=['POST'])
 @cross_origin(support_credentials=True)
 def sequence():
-    pdb_code = request.form['pdbcode']
-    PdbValidators.validate_pdb_code(pdb_code)
+    pdb_code = request.json['pdbcode']
 
     return get_sequence_from(pdb_code) if PdbValidators.validate_pdb_code(pdb_code) else ''
+
 
 @app.route('/homologousSequence', methods=['POST'])
 @cross_origin(support_credentials=True)
@@ -32,10 +32,11 @@ def homologous_sequences():
     result = [{'title': seq.get('title'), 'sequence': seq.get('sequence')} for seq in sequences.get('d')]
     return {'d': result}
 
+
 @app.route('/homologousSequenceDetails', methods=['POST'])
 @cross_origin(support_credentials=True)
 def homologous_sequences_details():
-    main_sequence = sequence();
+    main_sequence = sequence()
     print(main_sequence)
 
     records = blast_records(main_sequence)
