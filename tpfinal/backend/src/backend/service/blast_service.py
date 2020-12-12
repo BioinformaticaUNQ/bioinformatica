c2c517b0e246d663c82203bb24f5a17d94a93557
Fabrizio <fabri1108@gmail.com>
2020-12-11 21:09:13 -0500

    cambios visuales en la lista de seleccion de secuencias

--- tpfinal/backend/src/backend/service/blast_service.py
+++ tpfinal/backend/src/backend/service/blast_service.py
@@ -69,15 +69,16 @@ class BlastService:
         for record in records:
             for alignment in record.alignments:
                 if self.is_identity_and_coverage_valid(alignment, query_len, coverage_expected) :
-                    try:
-                        results.append({
-                            'title': alignment.title.split('>')[0],
-                            'sequence': PDBService.get_sequence(alignment.hit_id.split('|')[1], alignment.hit_id.split('|')[2])
-                        })
-                    except:
-                        print('secuencia ignorada por error en pbdserivce.')
+
+                    results.append({
+                        'title': alignment.title.split('>')[0],
+                        'sequence': PDBService.get_sequence(alignment.hit_id.split('|')[1], alignment.hit_id.split('|')[2])
+                    })
+
                 else:
                     print(alignment.hit_id.split('|')[1], 'filtered')
+
+        return results
 
     def is_identity_and_coverage_valid(self, alignment, query_len, coverage_expected):
 
